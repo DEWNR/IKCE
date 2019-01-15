521141395cecbdbf1d6b9b836dbb43058c1fd30b
Liam Rella <Liam.Rella@sa.gov.au>
2019-01-15 15:50:07 +1030

    Set up new fonts

--- src/javascript/font-loader.js
+++ src/javascript/font-loader.js
@@ -2,20 +2,21 @@ import FontFaceObserver from 'fontfaceobserver/fontfaceobserver.standalone';
 
 const html = document.documentElement;
 
-const fontA = new FontFaceObserver('Dewnrger');
-const fontB = new FontFaceObserver('Frutiger');
+const fontA = new FontFaceObserver('Open Sans');
+const fontB = new FontFaceObserver('Adelle Condensed');
+const fontC = new FontFaceObserver('Proxima Nova Condensed');
 
 if (!sessionStorage.fontsLoaded) {
   Promise.all([
     // load Dewnrger
     fontA.load()
   ]).then(() => {
-    html.classList.add('fonts-stage-1');
-    // load Frutiger
     fontB.load().then(() => {
-      html.classList.add('fonts-stage-2');
-      // add token to session storage
-      sessionStorage.fontsLoaded = true;
+      fontC.load().then(() => {
+        html.classList.add('fonts-loaded')
+        // add token to session storage
+        sessionStorage.fontsLoaded = true;
+      });
     });
   }).catch((err) => {
     // remove token from session storage
@@ -24,5 +25,5 @@ if (!sessionStorage.fontsLoaded) {
   });
 } else {
   // Add all font classes to html tag
-  html.classList.add('fonts-stage-1', 'fonts-stage-2');
+  html.classList.add('fonts-loaded');
 };
