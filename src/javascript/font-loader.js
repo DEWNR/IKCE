01c9931c559dfe9a3793db3e16c8811116dc80b4
Liam Rella <Liam.Rella@sa.gov.au>
2019-01-16 11:25:54 +1030

    Font loading fixes

--- src/javascript/font-loader.js
+++ src/javascript/font-loader.js
@@ -3,17 +3,20 @@ import FontFaceObserver from 'fontfaceobserver/fontfaceobserver.standalone';
 const html = document.documentElement;
 
 const fontA = new FontFaceObserver('Open Sans');
-const fontB = new FontFaceObserver('Adelle Condensed');
-const fontC = new FontFaceObserver('Proxima Nova Condensed');
+const fontB = new FontFaceObserver('adelle-condensed');
+const fontC = new FontFaceObserver('proxima-nova-condensed');
 
 if (!sessionStorage.fontsLoaded) {
   Promise.all([
-    // load Dewnrger
+    // load Open Sans
     fontA.load()
   ]).then(() => {
+    // load Adelle
     fontB.load().then(() => {
+      // load Proxima
       fontC.load().then(() => {
-        html.classList.add('fonts-loaded')
+        // add class to html
+        html.classList.add('fonts-loaded');
         // add token to session storage
         sessionStorage.fontsLoaded = true;
       });
@@ -21,6 +24,7 @@ if (!sessionStorage.fontsLoaded) {
   }).catch((err) => {
     // remove token from session storage
     sessionStorage.fontsLoaded = false;
+    // send error to console
     console.error('Error: ', err);
   });
 } else {
